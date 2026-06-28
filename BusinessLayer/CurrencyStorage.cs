b4ba83c9019bf2786e5657328e4bd68bda40fb50
NickSlm <solomonovnick@gmail.com>
2026-06-28 20:35:32 +0300

    changes based on code review

---
+++ BusinessLayer/CurrencyStorage.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class CurrencyStorage: ICurrencyStorage
+    {
+        public event Action<IList<CurrencyPairs>>? PairsUpdated;
+        private readonly List<CurrencyPairs> _pairs = new();
+        public IList<CurrencyPairs> Pairs => _pairs;
+
+        public void Load(IList<CurrencyPairs> pairs)
+        {
+            _pairs.AddRange(pairs);
+            PairsUpdated?.Invoke(_pairs);
+        }
+
+        public void Update()
+        {
+            PairsUpdated?.Invoke(_pairs);
+        }
+    }
+}
---
+++ BusinessLayer/ICurrencyStorage.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ICurrencyStorage
+    {
+        event Action<IList<CurrencyPairs>>? PairsUpdated;
+        IList<CurrencyPairs> Pairs { get; }
+        void Load(IList<CurrencyPairs> pairs);
+        void Update();
+
+    }
+}
